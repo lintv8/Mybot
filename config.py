f4fc9ce99fa940b4a06acea8753bad54e557e2f0
LinTV频道 <63519688+lintv8@users.noreply.github.com>
2025-07-26 16:20:40 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
+from telegram.ext import CommandHandler, CallbackQueryHandler
+import os
+
+TOKEN = os.getenv('TOKEN')
+
 from handlers import start, button
 
-start_handler = start
-button_handler = button
+start_handler = CommandHandler("start", start)
+button_handler = CallbackQueryHandler(button)
